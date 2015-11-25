9d568f7ddc2a04afa93f35408dccaed2290d2dad
Mr. Super <linzechao44@163.com>
2015-11-25 18:18:45 +0800

    添加访问常量方法

---
+++ smarty/main/01.php
@@ -0,0 +1,20 @@
+<?
+    # 引入实例化
+    require_once('./libs/Smarty.class.php');
+    $smarty = new Smarty;
+
+    # 定界符配置
+    # $smarty -> left_delimiter = '{{';
+    # $smarty -> right_delimiter = '}}';
+    # 文件夹配置
+    # $smarty -> setTemplateDir('./view');
+    # $smarty -> setCompileDir('./view_c');
+
+    # 传入参数
+    $smarty -> assign('title', '01.html');
+    $smarty -> assign('header', '01.html');
+    $smarty -> assign('about', '这是一个01.html页面。');
+
+    # 显示页面
+    # 默认templates目录
+    $smarty -> display('01.html');
---
+++ smarty/main/02.php
@@ -0,0 +1,17 @@
+<?
+    # 引入实例化
+    require_once('./libs/Smarty.class.php');
+    $smarty = new Smarty;
+
+    # 常量
+    define('HOST', 'http://www.sc.com');
+
+    # 使用全局变量
+    # $_GET, $_POST, $_FILES, $GLOBALS, $_COOKIE, $_SESSION, $SEVSER, $REQUEST, $_ENV, 常量信息
+    # $GLOBALS, $_FILES不能用
+
+    $smarty -> assign('name', $_GET['name']);
+
+
+
+    $smarty -> display('02.html');
