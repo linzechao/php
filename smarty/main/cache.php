3c78b3c8c2209e701511e216b7eb92ca216b1494
Mr. Super <linzechao44@163.com>
2015-11-26 16:30:48 +0800

    添加缓存笔记

--- smarty/main/cache.php
+++ smarty/main/cache.php
@@ -14,9 +14,13 @@
     $smarty -> caching = 1; # 使用cache_lifetime时间来判断
     $smarty -> caching = 2; # 根据缓存文件自身的时间判断，前面注释代码里，在程序中修改cache_lifetime不会影响缓存时间
 
+    $smarty -> force_cache = true; // 强制访问缓存
+
+    
     # 修改固定时间刷新
     # 单位秒
     $smarty -> cache_lifetime = 20;
+
 
     # 单个变量不缓存
     # {$title nocache}
@@ -26,18 +30,23 @@
     # 局部不缓存
     # {nocache}...{/nocache}
 
-    # 缓存集合
     $a = $_GET['a'];
     $b = $_GET['b'];
     $c = $_GET['c'];
     $d = $_GET['d'];
 
 
+    # 缓存集合
+    $smarty -> display('07.html', $a.'|'.$b.'|'.$c.'|'.$d);
+
+    // $smarty -> clearAllCache(); // 暴力清除所有缓存
+    // $smarty -> clearCache(null, $a); // 清除标记缓存
+
 
     # 假如是分页
-    $page = 4;
+    # $page = 4;
     # display(模板, 标记)
-    $smarty -> display('07.html', $page);
+    # $smarty -> display('07.html', $page);
 
     # 是否已经缓存了
     # $smarty -> isCached('07.html');
